a396352959530d5f0651d2a5d477a232ee6a2648
say-saha <sayansaha133@gmail.com>
2023-01-15 23:19:04 +0100

    Update website to generate report

--- Website/bfv_enc.js
+++ Website/bfv_enc.js
@@ -621,65 +621,64 @@ class BFVParameters {
 }
 
 function enc(){
-      var degrees = [8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096];
-      for (let i=0;i<degrees.length;i++){ 
-            var startTime = performance.now();
-            let degree = degrees[i];
-            let plain_modulus = 7;
-            let cipher_modulus = 8000000000000;
-            const params = new BFVParameters(degree, plain_modulus, cipher_modulus);
-            const key_generator = new BFVKeyGenerator(params);
-                  
-            const secret_key = key_generator.secret_key;
-            const public_key = key_generator.public_key;
-            const relin_key = key_generator.relin_key;
+      var working = "No"
+      var test_case = "x*y";
+      var startTime = performance.now();
+      let degree = 2048;
+      let plain_modulus = 7;
+      let cipher_modulus = 8000000000000;
+      const params = new BFVParameters(degree, plain_modulus, cipher_modulus);
+      const key_generator = new BFVKeyGenerator(params);
             
-            const encoder = new IntegerEncoder(params);
-            const encryptor = new BFVEncryptor(params, public_key);
-            const decryptor = new BFVDecryptor(params, secret_key);
-            const evaluator = new BFVEvaluator(params);
-            let count = 0;
-            let time = 0;
-            let message1 = Math.floor(Math.random() * 5);
-            // let message1 = 5;
-            // console.log("Number 1: ", message1);
-            let message2 = Math.floor(Math.random() * 5);
-            // let message2 = 5;
-            // console.log("Number 2: ", message2);
-            let message3 = Math.floor(Math.random() * 5);
-            // // let message3 = 2;
-            // console.log("Number 3: ", message3);
+      const secret_key = key_generator.secret_key;
+      const public_key = key_generator.public_key;
+      const relin_key = key_generator.relin_key;
+      var key_gen_time = (performance.now() - startTime)/100;
       
-            let encoded_plain1 = encoder.encode(message1);
-            let encoded_plain2 = encoder.encode(message2);
-            let encoded_plain3 = encoder.encode(message3);
+      let message1 = Math.floor(Math.random() * 100);
+      let message2 = Math.floor(Math.random() * 100);
+      let message3 = Math.floor(Math.random() * 100);
 
-            let cipher1 = encryptor.encrypt(encoded_plain1);
-            let cipher2 = encryptor.encrypt(encoded_plain2);
-            let cipher3 = encryptor.encrypt(encoded_plain3);
+      const encoder = new IntegerEncoder(params);
+      const encryptor = new BFVEncryptor(params, public_key);
 
-            // let final = evaluator.multiply(cipher1, cipher2, relin_key);
-            // final = evaluator.multiply(final, cipher3, relin_key);
-            let final = evaluator.multiply(cipher1, cipher2, relin_key);
-            final = evaluator.add(final, cipher3);
-            // let final = evaluator.add(cipher1, cipher2);
-            // console.log("Cipher text: ", final);
-            let decrypted_encoded = decryptor.decrypt(final);
-            // console.log("Decrypted encoded: ", decrypted_encoded);
-            let decrypted = encoder.decode(decrypted_encoded);
-            // console.log("Decrypted: ", decrypted);
-            // if(message1 + message2 + message3 == decrypted)
-            //       count ++;
-            console.log("Degree: ", degrees[i]);
-            if((message1 * message2) + message3 == decrypted)
-                  console.log("Working: YES");
-            else
-                  console.log("Working: NO");
-            var endTime = performance.now();
-            time = endTime - startTime;
-            
-            // console.log("Count: ", count);
-            console.log(`Average time taken ${time/100} milliseconds`)  
-      }
+      let encoded_plain1 = encoder.encode(message1);
+      let encoded_plain2 = encoder.encode(message2);
+      let encoded_plain3 = encoder.encode(message3);
+
+      let cipher1 = encryptor.encrypt(encoded_plain1);
+      let cipher2 = encryptor.encrypt(encoded_plain2);
+      let cipher3 = encryptor.encrypt(encoded_plain3);
+      var encryption_time = ((performance.now() - startTime)/100) - key_gen_time;
+
+      const evaluator = new BFVEvaluator(params);
+      let final = evaluator.multiply(cipher1, cipher2, relin_key);
+      // final = evaluator.multiply(final, cipher3, relin_key);
+      var operation_time = ((performance.now() - startTime)/100) - (key_gen_time + encryption_time);
+
+      const decryptor = new BFVDecryptor(params, secret_key);
+      let decrypted_encoded = decryptor.decrypt(final);
+      let decrypted = encoder.decode(decrypted_encoded);
+      var decryption_time = ((performance.now() - startTime)/100) - (key_gen_time + encryption_time + operation_time);
+
+      if(message1 * message2 == decrypted)
+            working = "YES";
+
+      var total_time = (performance.now() - startTime)/100;            
+
+      document.getElementById("degree").innerHTML = degree;
+      document.getElementById("plain_modulus").innerHTML = plain_modulus;
+      document.getElementById("cipher_modulus").innerHTML = cipher_modulus;
+      document.getElementById("test_case").innerHTML = test_case;
+      document.getElementById("number_1 (x)").innerHTML = message1;
+      document.getElementById("number_2 (y)").innerHTML = message2;
+      document.getElementById("number_3 (z)").innerHTML = message3;
+      document.getElementById("key_gen_time").innerHTML = (key_gen_time.toFixed(3)).toString() + " ms";
+      document.getElementById("encryption_time").innerHTML = (encryption_time.toFixed(3)).toString() + " ms";
+      document.getElementById("operation_time").innerHTML = (operation_time.toFixed(3)).toString() + " ms";
+      document.getElementById("decryption_time").innerHTML = (decryption_time.toFixed(3)).toString() + " ms";
+      document.getElementById("output").innerHTML = decrypted;
+      document.getElementById("working").innerHTML = working;
+      document.getElementById("total_time").innerHTML = (total_time.toFixed(3)).toString() + " ms";
 }
 
